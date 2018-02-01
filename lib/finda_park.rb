899e30dad17fb1a85be91a66a5a0763c6fe5eb15
ElleeB <laura.trager@gmail.com>
2018-02-01 15:01:50 -0500

    Second Basic Update

--- lib/finda_park.rb
+++ lib/finda_park.rb
@@ -1,5 +1,5 @@
 ## Requires all other files
-require "finda_park/version"
+require_relative "./finda_park/version"
 require_relative './cli.rb'
 require_relative './park.rb'
 require_relative './state.rb'
