007d3335d5f0b810ae270d6904932cadf585d39f
ElleeB <laura.trager@gmail.com>
2018-02-02 13:56:30 -0500

    Park class methods update

--- lib/park.rb
+++ lib/park.rb
@@ -1,3 +1,24 @@
 class FindaPark::Park
-  # attr_accessor :name, :state, :city, :contact, :blurb, :url, :catch_phrase, :season_info, :hours
+
+  attr_accessor :name, :state, :city, :contact, :blurb, :url, :catch_phrase, :season_info, :hours
+
+  @@all_parks
+
+  def initialize
+    # assign attributes
+    # save
+  end
+
+  def self.create_from_collection
+    # use collection of parks to instantiate instances of parks
+    # assign designation, name, city, blurb, url
+  end
+
+  def add_attributes
+    # add catch phrase, contact, info url
+  end
+
+  def add_season_info_hours
+  end
+
 end
--- lib/state.rb
+++ lib/state.rb
@@ -1,3 +1,29 @@
 class FindaPark::State
-  # attr_accessor :name, :park
+
+  attr_accessor :name, :park, :url
+
+  @@all_states = []
+
+  @parks = []
+
+  def initialize
+    # define attributes
+    # add to @@all_states - .save
+  end
+
+  def self.create_from_collection
+    # use collection of states from scraper to instantiate instances of states
+    # assign name & url
+  end
+
+  def add_parks
+    # use collection of state's parks to instantiate instances of parks
+    # add to @parks
+  end
+
+  def save
+    @@all_states << self
+  end
+
+
 end
