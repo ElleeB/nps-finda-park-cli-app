d3d2e39dbb63622b0e628e29ff71ebbc59a82ce4
ElleeB <laura.trager@gmail.com>
2018-02-01 21:48:06 -0500

    Updated initial scraper and outlined methods.

--- lib/park.rb
+++ lib/park.rb
@@ -1,3 +1,3 @@
 class FindaPark::Park
-  # attr_accessor :name, :state, :city, :address, :phone, :blurb, :url
+  # attr_accessor :name, :state, :city, :address, :phone, :blurb, :url, :catch_phrase, :season, :hours
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,30 @@
 class FindaPark::Scraper
 
-  @states_array = []
-
   def self.index_scraper(index_url)
+    @states_array = []
     doc = Nokogiri::HTML(open(index_url))
     states = doc.css("select#state").search("option")
-    states.each{ |s| @states_array << s.text}
+    # => <option value="or">Oregon</option>
+    states.each do |s|
+      state_hash = {:name => nil, :url => nil}
+      state_hash[:name] = s.text
+      state_hash[:url] = "https://www.nps.gov/state/#{s.attribute("value")}/index.htm"
+      @states_array << state_hash
+    end
+    @states_array.shift
     @states_array
   end
+
+  def self.state_parks_scraper(state_url)
+    state_park_hash = {:designation => nil, :name => nil, :city => nil, :blurb => nil, :url => nil}
+  end
+
+    def park_scraper(park_url)
+      park_hash = {:catch_phrase => nil, :address => nil, :phone => nil, :info_url => nil}
+    end
+
+    def info_scraper(info_url)
+      #obtain seasons and hours
+    end
 
 end
