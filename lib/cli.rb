312b7252e275dac5c78bdcb5d811dd1ac2342199
ElleeB <laura.trager@gmail.com>
2018-02-01 20:38:30 -0500

    Outline  cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,16 @@
 class FindaPark::CLI
 
+  # structure
+
+  # greet user
+  # list states
+  # ask for input (state selection)
+  # present list of parks
+  # ask for input (park selection)
+  # display park blurb and contact info
+
+  # methods
+
   # call
   # run
   # make parks
